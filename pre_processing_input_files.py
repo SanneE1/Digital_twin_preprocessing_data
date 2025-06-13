f386bdf81aeb6bda6261ef9849fdf8a5c4604e2b
Sanne Evers <sanne.m.evers@gmail.com>
2025-06-13 19:15:11 +0200

    consolidate lynx and rabbit preprocessing files into one

--- pre_processing_input_files.py
+++ pre_processing_input_files.py
@@ -11,6 +11,9 @@ import rpy2.robjects as robjects
 # Reclassify CORINE map for rabbit suitability - for some reason the robjects.r.source() doesn't work here, but subprocess works just fine!
 robjects.r.source('R/Reclassify_Rabbit_spatial_map.R')
 
+# Reclassify CORINE map for lynx
+robjects.r.source('R/Reclassify_Lynx_spatial_map.R')
+
 # Change maps to input format needed for model
 subprocess.run(["python", "python/transform_asc_to_input_txt_map.py"],
                capture_output=True)
