c3bd34c7650ac9f099ef1d5043d7af994c3211c0
Sanne Evers <sanne.m.evers@gmail.com>
2025-08-18 14:07:14 +0200

    update slurm script for formatting future climate after changing file structure

--- R/Format_climate_data_for_rabbit_slurm_batch.R
+++ R/Format_climate_data_for_rabbit_slurm_batch.R
@@ -3,38 +3,28 @@ source("R/Formate_climate_data_for_rabbit.R")
 # Get arguments from command line
 args <- commandArgs(trailingOnly = TRUE)
 
-array_id <- as.integer(args[1])
-total_arrays <- as.integer(args[2])
 
-download_location <- sub("/$", "", args[3])
-year_min <- as.integer(args[4])
-year_max <- as.integer(args[5])
+download_location <- sub("/$", "", args[1])   # remove traling / in case it's added on submission
 
-if (args[6] == "peninsula") {
+a <- strsplit(download_location, "/")[[1]]
+result_dir <- paste("final_files", a[length(a)], sep = "/")
+
+year_min <- as.integer(args[2])
+year_max <- as.integer(args[3])
+
+if (args[4] == "peninsula") {
   coord_file = "data/coordinates_peninsula_500_EPSG4326.txt" 
-} else if (args[6] == "donana") {
+} else if (args[4] == "donana") {
   coord_file = "data/coordinates_donana_500_EPSG4326.txt" 
 } else {
   stop("files for coordinates only have two options: 'donana' or 'peninsula'")
 }
 
-# Read the coordinate file to get total number of rows
-total_rows <- nrow(read.table(coord_file))
-
-# Calculate chunk size and start/end rows
-chunk_size <- ceiling(total_rows / total_arrays)
-start_row <- (array_id - 1) * chunk_size + 1
-end_row <- min(array_id * chunk_size, total_rows)
-
-# Extract row index ranges
-row_indices <- c(start_row:end_row)
 
 calculate_BM_cDM(download_location = download_location, 
+                 coord_file = coord_file, 
                  year_min = year_min,
                  year_max = year_max,
-                 coord_file = coord_file, 
-                 row_indices = row_indices)
-
-
-
+                 result_dir = result_dir,
+                 temperature_type = "minmax")
 
--- submission_future_climate_3_format_climate.sh
+++ submission_future_climate_3_format_climate.sh
@@ -2,8 +2,7 @@
 #SBATCH --job-name=format_nc        # Job name
 #SBATCH --nodes=1                   # Request one node
 #SBATCH --ntasks=1                  # Run a single task
-#SBATCH --array=1-1000
-#SBATCH --mem=3G                   # Request 8 GB of memory
+#SBATCH --mem=3G                   # Request ... GB of memory
 #SBATCH --output=job_reports/format_output_%j.log        # Standard output and error log (%j expands to jobID)
 #SBATCH --error=job_reports/format_error_%j.log
 
@@ -13,4 +12,4 @@ YEAR_MAX=$3
 SPAT_OPTION=$4
 
 # Run wget download line
-Rscript R/Format_climate_data_for_rabbit_slurm_batch.R $SLURM_ARRAY_TASK_ID 1000 $DOWN_DIR $YEAR_MIN $YEAR_MAX $SPAT_OPTION
+Rscript R/Format_climate_data_for_rabbit_slurm_batch.R $DOWN_DIR $YEAR_MIN $YEAR_MAX $SPAT_OPTION
